c50da2a5f321bab96d344adfeba34bad28470e1b
saurav <saurav@BRIMACLAP055.lan>
2019-12-16 08:31:43 +0530

    set exp

--- code/train.py
+++ code/train.py
@@ -17,7 +17,6 @@ import mlflow.sklearn
 
 import logging
 mlflow.set_tracking_uri('http://52.230.49.126:5000')
-mlflow.create_experiment('Training with elasticnet')
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
@@ -46,7 +45,7 @@ if __name__ == "__main__":
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
-
+    mlflow.set_experiment('Training with elasticnet')
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
