e8afa8db1dfea2e8333674bacf961cbbef0823d5
saurav <saurav@BRIMACLAP055.lan>
2020-01-21 16:49:10 +0530

    remove print

--- code/split_data.py
+++ code/split_data.py
@@ -10,7 +10,6 @@ np.random.seed(40)
 
 # Read the csv file from the data folder 
 data = pd.read_csv('./df.csv', sep=';')
-print(data.head())
 
 # Split the data into training and test sets. (0.75, 0.25) split.
 train, test = train_test_split(data)
