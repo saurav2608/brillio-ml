4d586448ce204a8784caa62a2508d7cfd5a1ecfc
saurav <saurav@BRILAP1428.BRILLIO.COM>
2020-01-21 12:33:18 +0530

    changes

--- code/data.py
+++ code/data.py
@@ -9,6 +9,9 @@ container_name = 'data'
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 source_file_name = 'semantic_layer/final_data.csv'
 
+# Create a blob client using the local file name as the name for the blob
+blob_downlaod_client = blob_service_client.get_blob_client(container=container_name, blob=source_file_name)
+
 with open('~/data_trg/df.csv') as my_blob:
     blob_data = blob_downlaod_client.download_blob()
     blob_data.readinto(my_blob)
