e949025d95700c21d717ca0ac6f6417f26fd1837
saurav <saurav@BRIMACLAP055.lan>
2020-01-21 14:21:11 +0530

    include all tests

--- train.sh
+++ train.sh
@@ -64,4 +64,7 @@ conda env create -q --file=envs/code.yml
 echo "activate code"
 conda activate code
 
+python3 code/data.py
+python3 code/split_data.py
 python3 code/train.py .8 .7
+python -m pytest
